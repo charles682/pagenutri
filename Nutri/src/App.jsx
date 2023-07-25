f95a3ed4796e3b8eb0385e700c2e43b5257bacde
Charles Melo <melocharles220@gmail.com>
2023-07-25 09:08:10 -0300

    update scroll

--- Nutri/src/App.jsx
+++ Nutri/src/App.jsx
@@ -7,9 +7,26 @@ import {Servico} from './pages/Serviços';
 import {Depoimentos} from './pages/Depoimentos';
 import {Ebook} from './pages/Ebook';
 import {Contato} from './pages/Contato';
+import React, { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 
 
 function App() {
+    useEffect(() => {
+      const scrollReveal = ScrollReveal({
+        origin: 'top',
+        distance: '30px',
+        duration: 500,
+        reset: true
+      });
+  
+      scrollReveal.reveal(".text, p, .title", { interval: 100 });
+  
+      // Limpar ScrollReveal ao desmontar o componente
+    return () => {
+        scrollReveal.destroy(0);
+      };
+    }, []);
   return (
   <Container>
    <Header  />
