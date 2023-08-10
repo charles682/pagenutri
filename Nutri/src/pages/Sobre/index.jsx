ae3ce33aeafbeb79298bd0b41694cfe9eefd1b6c
Charles Melo <melocharles220@gmail.com>
2023-08-10 14:15:19 -0300

    upadate text

--- Nutri/src/pages/Sobre/index.jsx
+++ Nutri/src/pages/Sobre/index.jsx
@@ -19,17 +19,17 @@ export function Sobre(){
           <div className="text">
             <h2 className="title">Sobre</h2>
           
-            <p>Cristiane Umbelina - Nutricionista Esportiva</p>
+            <p>Cristiane Umbelina - Nutricionista Clínica Esportiva</p>
            
             <h2>
               Minha paixão é ajudar as pessoas a emagrecerem e ganhar massa magra de forma saudável, sem radicalismo e sem sofrimento.
             </h2>
             <p>
-              Sou nutricionista há 4 anos e me especializei em Nutrição Clínica E Esportiva. Meu principal foco é ajudar as pessoas no processo de emagrecimento, hipertrofia muscular, e melhor qualidade de vida para que conquistem seus objetivos estéticos e de saúde, e assim tenham a autoestima que desejarem e possam usufruir da vida com mais saúde e qualidade de vida.
+              Sou bacharela em Nutrição, formada a 5 anos pelo Centro Universitário Una, em Belo Horizonte-MG. Me especializei em Nutrição Clínica E Esportiva. Meu principal foco é ajudar as pessoas a terem uma melhor qualidade de vida através da prevenção de doenças, e cuidados no processo de emagrecimento, hipertrofia muscular, e também no controle e tratamento de doenças já existentes. Assim, com base em um protocolo nutricional correto, todos possam alcançar seus objetivos estéticos e de saúde, e assim ter a autoestima que desejar e poder usufruir da vida com mais saúde e qualidade de vida.
             </p>
             <br />
             <p>
-              Acredito que a dieta deve se adequar a rotina do paciente, com alimentação simples e acessível, pois dessa forma o plano alimentar se torna mais fácil e contribui para alcançar os objetivos desejados.
+              Acredito que uma boa dieta deve se adequar a rotina do paciente, com alimentação simples e acessível, pois dessa forma o plano alimentar se torna mais fácil de ser szeguido e contribui para alcançar os objetivos desejados; uma vez que nossos hábitos alimentares refletem em toda a nossa saúde como um todo. 
             </p>
           </div>
         </div>
